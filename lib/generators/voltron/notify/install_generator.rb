2dfd6bb89a66fa658b4c45e46a4704406bc091f6
Eric Hainer <eric.hainer@gmail.com>
2016-09-22 13:18:41 -0600

    Ensure migration ids are different

--- lib/generators/voltron/notify/install_generator.rb
+++ lib/generators/voltron/notify/install_generator.rb
@@ -51,10 +51,15 @@ CONTENT
 				end
 
 				def copy_migrations
-					copy_file "db/migrate/create_voltron_notifications.rb", Rails.root.join("db", "migrate", "#{Time.now.strftime("%Y%m%d%H%M%S")}_create_voltron_notifications.rb")
-					copy_file "db/migrate/create_voltron_notification_sms_notifications.rb", Rails.root.join("db", "migrate", "#{Time.now.strftime("%Y%m%d%H%M%S")}_create_voltron_notification_sms_notifications.rb")
-					copy_file "db/migrate/create_voltron_notification_email_notifications.rb", Rails.root.join("db", "migrate", "#{Time.now.strftime("%Y%m%d%H%M%S")}_create_voltron_notification_email_notifications.rb")
-					copy_file "db/migrate/create_voltron_notification_sms_notification_attachments.rb", Rails.root.join("db", "migrate", "#{Time.now.strftime("%Y%m%d%H%M%S")}_create_voltron_notification_sms_notification_attachments.rb")
+					copy_file "db/migrate/create_voltron_notifications.rb", Rails.root.join("db", "migrate", "#{migration_time}_create_voltron_notifications.rb")
+					copy_file "db/migrate/create_voltron_notification_sms_notifications.rb", Rails.root.join("db", "migrate", "#{migration_time}_create_voltron_notification_sms_notifications.rb")
+					copy_file "db/migrate/create_voltron_notification_email_notifications.rb", Rails.root.join("db", "migrate", "#{migration_time}_create_voltron_notification_email_notifications.rb")
+					copy_file "db/migrate/create_voltron_notification_sms_notification_attachments.rb", Rails.root.join("db", "migrate", "#{migration_time}_create_voltron_notification_sms_notification_attachments.rb")
+				end
+
+				def migration_time
+					@migration_id ||= Time.now.strftime("%Y%m%d%H%M%S").to_i
+					@migration_id += 1
 				end
 			end
 		end
