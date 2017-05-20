7a46bd95f6ee1bdaed65043768e8b58907d1954c
Eric Hainer <eric.hainer@gmail.com>
2017-05-19 23:46:55 -0600

    [CHG] Locale copy change

--- spec/mailers/voltron/notification_mailer_spec.rb
+++ spec/mailers/voltron/notification_mailer_spec.rb
@@ -1,25 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe Voltron::NotificationMailer, type: :mailer do
 
   let(:mailer) { Voltron::NotificationMailer }
 
-  it "should pass the first hash of arguments as mail arguments" do
+  it 'should pass the first hash of arguments as mail arguments' do
 
-    mailer.notify({ to: "test@example.com", subject: "Test" }).deliver_now
+    mailer.notify({ to: 'test@example.com', subject: 'Test' }).deliver_now
 
     last_email = ActionMailer::Base.deliveries.last
 
-    expect(last_email.header["to"].value).to eq("test@example.com")
+    expect(last_email.header['to'].value).to eq('test@example.com')
 
-    expect(last_email.header["subject"].value).to eq("Test")
+    expect(last_email.header['subject'].value).to eq('Test')
   end
 
-  it "should convert the second hash of arguments to instance variables" do
+  it 'should convert the second hash of arguments to instance variables' do
 
-    mailer.notify({ to: "test@example.com", subject: "Test" }, { body: "Test Body" }).deliver_now
+    expect { mailer.notify({ to: 'test@example.com', subject: 'Test' }, { body: 'Test Body' }).deliver_now }.to change(ActionMailer::Base.deliveries, :count).by(1)
 
     last_email = ActionMailer::Base.deliveries.last
+
+    puts last_email.body
 
     expect(last_email.body.raw_source).to eq("<html>\n  <body>\n    Test Body\n  </body>\n</html>\n")
   end
